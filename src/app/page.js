4d42034f4ce54bc714345f72ffcc9ae21ea22d17
Brian Wanyama <brianwanyama06@gmail.com>
2025-02-19 15:55:18 +0300

    suspense added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import Header from "@/components/header";
 import Body from "@/components/body";
+import { Suspense } from "react";
 
 
 
@@ -11,7 +12,9 @@ export default function Home() {
   return (
     <>
     <Header/>
+    <Suspense>
     <Body/>
+    </Suspense>
     </>
   );
 }
